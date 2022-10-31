28bb33b17d825ae30d0763da7d371bf9c74c807a
PoggerPaul <114891770+21-PaulChong@users.noreply.github.com>
2022-10-31 15:27:45 +0800

    Update 3
    Fixed Error in TestSuite Script.

--- Lab10 - Project Starter/Assets/Tests/TestSuite.cs
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -14,18 +14,18 @@ namespace Tests
         {
             //Use the Assert class to test conditions
             result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);       
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityAddition()
+        {
+            //Use the Assert class to test conditions
+            //Use yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
             Assert.AreEqual(result, 7);
-
-            [UnityTest]
-            public IEnumerator TestUnityAddition()
-            {
-                //Use the Assert class to test conditions
-                //Use yield to skip a frame
-                yield return null;
-
-                result = Calculator.CalculatePair(5, 2, "+");
-                Assert.AreEqual(result, 7);
-            }
         }
     }
 }
